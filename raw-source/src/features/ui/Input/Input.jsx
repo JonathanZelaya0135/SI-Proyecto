995898ecb3a77fb7f3260741feea9071ea94ec2f
00402720 <00402720@uca.edu.sv>
2025-06-29 12:45:01 -0600

    Add: Profile Screen
    Added profile page with full functionality, and UI/UX changes

--- raw-source/src/features/ui/Input/Input.jsx
+++ raw-source/src/features/ui/Input/Input.jsx
@@ -1,9 +1,10 @@
 import './Input.css'
 
-export default function Input({ type, placeholder, value, onChange }) {
+export default function Input({ name, type, placeholder, value, onChange }) {
   return (
     <div className="input-group">
       <input
+        name={name}
         type={type}
         value={value}
         onChange={onChange}
