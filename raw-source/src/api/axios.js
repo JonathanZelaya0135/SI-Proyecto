fe66fa7a86a27f455ee3de2bc592c7fc15c8e0e7
JonathanZelaya0135 <00403821@uca.edu.sv>
2025-11-21 15:15:46 -0600

    fix: backend url

--- raw-source/src/api/axios.js
+++ raw-source/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://rawsourceapi.up.railway.app/api',
+  baseURL: 'https://resourceapi-g59x.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
